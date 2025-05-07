4ac68e1ea9a1cac7ca8325d98d9b3bb07aae8eeb
whyuhurtz <78311798+whyuhurtz@users.noreply.github.com>
2025-05-07 21:40:29 +0700

    Hapus yang unusable.

--- backend/dtos/dto_firmware.py
+++ backend/dtos/dto_firmware.py
@@ -5,23 +5,12 @@ from dtos.dto_common import BasePage
 from models.model_firmware import Firmware
 
 
-# For POST, PUT (NODE_ID), AND DELETE (NODE_ID)
 class InputFirmware(BaseModel):
-    firmware_description: Optional[str] = Field(min_length=1)
-    firmware_version: str = Field(min_length=1, max_length=5, pattern=r'^\d+\.\d+\.\d+$') # MAJOR.MINOR.PATCH
+    firmware_description: Optional[str] = Field(min_length=1, max_length=255)
+    firmware_version: str = Field(min_length=1, max_length=8, pattern=r'^\d+\.\d+\.\d+$') # MAJOR.MINOR.PATCH
     firmware_url: str = Field(min_length=1, pattern=r'^(http|https)://.*$') # URL FORMAT
     node_id: int = Field(min=1)
     node_location: str = Field(min_length=1, max_length=255)
-
-    @field_validator("firmware_version")
-    def validate_firmware_version(cls, value):
-        if any(char.isdigit() for char in value):
-            raise ValueError("Firmware version must be in the format X.Y.Z")
-        return value
-    
-    @field_validator("firmware_url")
-    def validate_firmware_link(cls, value):
-        pass
 
     class Config:
         json_schema_extra ={
@@ -38,6 +27,7 @@ class InputFirmware(BaseModel):
 class FilterOptions(TypedDict):
     node_id: List[int]
     node_location: List[str]
+
 
 class OutputFirmwarePagination(BasePage):
     filter_options: FilterOptions = Field(default_factory=lambda: {"node_id": [], "node_location": []})
